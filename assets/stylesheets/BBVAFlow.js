dcee8368dd16566b198207e8804be09ee9d0a915
Manu Torres <manutorr@MacBook-Pro-de-Manu.local>
2018-09-08 23:03:40 -0500

    Fonts

---
+++ assets/stylesheets/BBVAFlow.js
@@ -0,0 +1,19 @@
+
+{
+"colors": {
+  "white":"#FFFFFF",
+  "liteBlue":"#49A5E6",
+  "medBlue":"#1464A5",
+  "deepBlue":"#2DCCCD",
+  "aquaBlue":"#072146",
+  "coolGray":"#E2E9EF",
+  "happyGreen"
+}
+
+colors = {
+	"boardColor":"#1464A5",
+	"boardFont":"BentonSans Thin",
+	"keypadSize":14,
+	"":""
+
+}
