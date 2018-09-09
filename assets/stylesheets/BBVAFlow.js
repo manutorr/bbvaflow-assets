5083ab4b070d33a2c7e20a1f4499f85b57ffc6f4
Manu Torres <manutorr@MacBook-Pro-de-Manu.local>
2018-09-09 00:43:05 -0500

    UX

--- assets/stylesheets/BBVAFlow.js
+++ assets/stylesheets/BBVAFlow.js
@@ -1,19 +1,19 @@
 
 {
 "colors": {
-  "white":"#FFFFFF",
-  "liteBlue":"#49A5E6",
-  "medBlue":"#1464A5",
-  "deepBlue":"#2DCCCD",
-  "aquaBlue":"#072146",
-  "coolGray":"#E2E9EF",
-  "happyGreen"
+  white:'#FFFFFF',
+  liteBlue:'#49A5E6',
+  medBlue:'#1464A5',
+  deepBlue:'#2DCCCD',
+  aquaBlue:'#072146',
+  coolGray:'#E2E9EF',
+  happyGreen:'#2dcccd',
+
 }
 
 colors = {
-	"boardColor":"#1464A5",
-	"boardFont":"BentonSans Thin",
-	"keypadSize":14,
-	"":""
+	boardColor:'#1464A5',
+	boardFont:'BentonSans Thin',
+	keypadSize:14,
 
 }
